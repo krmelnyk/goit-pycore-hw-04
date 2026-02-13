261ed79ace579aa775b9743196e10a76643ddfb5
kmelnyk <kyrylo.melnyk@uni-ops.com>
2026-02-13 20:47:15 +0200

    Remove 'main' from public interface exports in assistant package

--- src/assistant/__init__.py
+++ src/assistant/__init__.py
@@ -4,7 +4,6 @@ from .handlers import add_contact, change_contact, show_all, show_phone
 from .parser import parse_input
 
 __all__ = [
-    "main",
     "parse_input",
     "add_contact",
     "change_contact",
